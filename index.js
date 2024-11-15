3a4885ae3b087b10243fd23f04851c6af9afae2e
Kristen Connors <kdconnors15@gmail.com>
2024-11-14 18:52:33 -0700

    final commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let first = 5 * 2;
+let second = 39 + 1;
+let third = 40 - 1;
+
+window.alert(
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination: " +
+    first +
+    "-" +
+    second +
+    "-" +
+    third
+);
